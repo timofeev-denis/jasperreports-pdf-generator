a407692bdc2fccddaac95794bbb8baf4518dc0b5
Денис Тимофеев <denis.timofeev@mts.ru>
2024-06-26 19:00:26 +0300

    Экспорт в PDF

--- src/main/java/ru/code4fun/demo/jasperreports/Application.java
+++ src/main/java/ru/code4fun/demo/jasperreports/Application.java
@@ -2,6 +2,9 @@ package ru.code4fun.demo.jasperreports;
 
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.PdfExporterConfiguration;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,10 +23,17 @@ public class Application {
         ArrayList<DataBean> dataList = DataBeanList.getDataBeanList();
         JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
         Map parameters = new HashMap();
+        parameters.put("ReportTitle", "Отчёт о выборе победителей");
+        parameters.put("FilterClause", "");
+        parameters.put("OrderClause", "");
         try {
             final JasperReport jasperReport = JasperCompileManager.compileReport(sourceFileName);
             final JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, beanColDataSource);
             JasperExportManager.exportReportToHtmlFile(jasperPrint, "target/StyledTextReport.html");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "target/StyledTextReport.pdf");
+//            var exporter = new JRPdfExporter();
+//            var configuration = new SimplePdfExporterConfiguration();
+//            exporter.setConfiguration(configuration);
         } catch (JRException e) {
             e.printStackTrace();
         }
