879b738689ba70da3a5a21a8d7893d65873b2000
Денис Тимофеев <denis.timofeev@mts.ru>
2024-06-28 15:10:12 +0300

    Рефакторинг, замена картинки, добавление плагина для упрощения запуска

--- src/main/java/ru/code4fun/demo/jasperreports/Application.java
+++ src/main/java/ru/code4fun/demo/jasperreports/Application.java
@@ -1,34 +1,27 @@
 package ru.code4fun.demo.jasperreports;
 
-import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Application {
+
     public static void main(String[] args) throws JRException {
-        final Application application = new Application();
+        var application = new Application();
         application.generateReport();
     }
 
-    public void generateReport() {
-        var sourceFileName = "/home/denis/java/pdf-report/pdfreport/src/main/resources/report_template.jrxml";
-
-        DataBeanList DataBeanList = new DataBeanList();
-        ArrayList<DataBean> dataList = DataBeanList.getDataBeanList();
-        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
-        Map parameters = new HashMap();
-        parameters.put("REPORT_TITLE", "Отчёт о выборе победителей");
-        try {
-            final JasperReport jasperReport = JasperCompileManager.compileReport(sourceFileName);
-            final JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, beanColDataSource);
-            JasperExportManager.exportReportToHtmlFile(jasperPrint, "target/StyledTextReport.html");
-            JasperExportManager.exportReportToPdfFile(jasperPrint, "target/StyledTextReport.pdf");
-        } catch (JRException e) {
-            e.printStackTrace();
-        }
-
+    public void generateReport() throws JRException {
+        JRBeanCollectionDataSource data = new JRBeanCollectionDataSource(DataGenerator.generate());
+        var template = this.getClass().getClassLoader().getResourceAsStream("report_template.jrxml");
+        var jasperReport = JasperCompileManager.compileReport(template);
+        var parameters = new HashMap<String, Object>();
+        parameters.put("REPORT_TITLE", "Список пользователей");
+        var jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, data);
+        JasperExportManager.exportReportToPdfFile(jasperPrint, "target/report.pdf");
     }
 }
