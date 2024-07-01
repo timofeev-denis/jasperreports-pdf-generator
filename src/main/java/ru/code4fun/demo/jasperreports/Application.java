03894b6782796a1fefa0ac0462aab7a45005cf23
Денис Тимофеев <denis.timofeev@mts.ru>
2024-07-01 15:55:48 +0300

    Добавление блока "Открытая аккредитация"

--- src/main/java/ru/code4fun/demo/jasperreports/Application.java
+++ src/main/java/ru/code4fun/demo/jasperreports/Application.java
@@ -20,7 +20,11 @@ public class Application {
         var template = this.getClass().getClassLoader().getResourceAsStream("report_template.jrxml");
         var jasperReport = JasperCompileManager.compileReport(template);
         var parameters = new HashMap<String, Object>();
-        parameters.put("REPORT_TITLE", "Список пользователей");
+        parameters.put("REPORT_TITLE", "ОВП (Отчёт о выборе победителя) №25436 от 21.10.2024");
+        parameters.put("ORGANIZER", "ООО МТС МЕДИА");
+        parameters.put("CATEGORIES", "ТБ Сеть радиодоступа, Оборудование базовой станции (BTS 2G), Оборудование контроллера 2G, Оборудование базовой станции 3G, Оборудование контроллера 3G, Оборудование базовой станции LTE, Системы управления (Радиосеть), Оборудование Femto Cell, Оборудование Femtocell Core, ПАК для планирования и оптимизации сети, ЗИП радиосети, ПО и лицензии радиосеть, ТП и ремонт оборудования радиосети, Монтаж и ПНР контроллеров радиосети, Услуги BSS, Консалтинг (Радиосеть), Оборудование и ПО полевого тестирования, Услуги полевого тестирования, бенчмаркинг, ПО и лицензии для данных полевого тестирования");
+        parameters.put("OPEN_ACCREDITATION_NAME", "Предварительный квалификационный отбор на предоставление права участия в на предоставление права участия в на предоставление права участия в");
+//        parameters.put("OPEN_ACCREDITATION_NAME", "Предварительный квалификационный отбор на предоставление права участия в закупочных процедурах/биддингах на закупку и размещение РИМ Группы МТС на носителях индор и эмбиент рекламы (АЗС, торговые сети, торгово-развлекательные");
         var jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, data);
         JasperExportManager.exportReportToPdfFile(jasperPrint, "target/report.pdf");
     }
